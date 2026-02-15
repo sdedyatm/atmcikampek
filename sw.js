500cffa27f22866149659983576199f0298fd766
sdedyatm <11027164+sdedyatm@users.noreply.github.com>
2026-02-15 13:35:45 +0700

    Change cache version to 'atm-v5'
    Updated cache version to 'atm-v5' for major updates.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'atm-v4'; // Ubah versi ini setiap kali ada update besar
+const CACHE_NAME = 'atm-v5'; // Ubah versi ini setiap kali ada update besar
 const ASSETS_TO_CACHE = [
   '/',
   "./index.html",
